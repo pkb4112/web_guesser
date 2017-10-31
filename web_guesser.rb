8e4c0dd824e47042a382c0f23fbb4a022d42b88a
Peter Brandon <peterkbrandon@gmail.com>
2017-10-31 19:20:20 -0400

    I3: Color changing feedback

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,38 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
+def check_guess(guess,num)
+	guess = guess.to_i
+
+		case 
+				when guess > num + 5 
+						message = "Way too high!"
+						bgcolor = "red"
+				when guess < num - 5 
+						message = "Way too low!"
+						bgcolor = "blue"
+				when guess > num 
+						message = "Too high!"
+						bgcolor = "orange"
+				when guess < num 
+						message = "Too low!"
+						bgcolor = "teal"
+				when guess == num 
+						message = "You got it right!"
+						bgcolor = "green"
+		end
+		return message, bgcolor
+end
+
 num = rand(101)
+
 get '/' do
-	erb :index, :locals => {:num => num}
-   
+	  guess = params["guess"]
+		message = check_guess(guess,num)[0]
+		bgcolor = check_guess(guess,num)[1]
+
+
+		erb :index, :locals => {:num => num, :message => message, :bgcolor => bgcolor}
 end
