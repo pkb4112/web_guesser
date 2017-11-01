c0a72ae35eca5642ce292f0c897d648e70826d61
Peter Brandon <peterkbrandon@gmail.com>
2017-10-31 20:59:39 -0400

    I4: cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -26,13 +26,37 @@ def check_guess(guess,num)
 		return message, bgcolor
 end
 
-num = rand(101)
+def generate_num
+ num = rand(101)
+ return num
+end
+
+def new_guess 
+	@@guesses = 5
+	num = generate_num
+	return num
+end
+
+num = new_guess
+
 
 get '/' do
 	  guess = params["guess"]
 		message = check_guess(guess,num)[0]
 		bgcolor = check_guess(guess,num)[1]
-
-
-		erb :index, :locals => {:num => num, :message => message, :bgcolor => bgcolor}
+		  @@guesses -= 1
+		if params["cheat"] == "true"
+	  	cheat = "Secret number is #{num}"
+	  end
+		
+	  if guess.to_i == num
+	  message = "You Win!" 
+	  	num = new_guess
+	  elsif @@guesses == 0 
+	  	message = "You Lose"
+	  	num = new_guess
+	  end
+    
+		
+		erb :index, :locals => {:num => num, :message => message, :bgcolor => bgcolor,:guesses => @@guesses, :cheat => cheat}
 end
